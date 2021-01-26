b45b37bc0070288b16cc42a380ba1e0c5acbc34c
huangxinhere <1159390360@qq.com>
2021-01-26 16:57:04 +0800

    texttoo

--- app/src/main/java/com/example/library/MainActivity.java
+++ app/src/main/java/com/example/library/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
 
-        ////text here
+        ////text here..
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
